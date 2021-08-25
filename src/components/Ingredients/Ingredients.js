2747a613a8e5fdd4cce1c615a7817fe2aae6b0f1
Christian <71664957+Christian-Mar@users.noreply.github.com>
2021-08-25 05:45:10 +0200

    useEffect added

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -6,15 +6,37 @@ import Search from './Search';
 
 const Ingredients = () => {
 	const [userIngredients, setUserIngredients] = useState([]);
+	useEffect(() => {
+		fetch(
+			'https://react-learning-hooks-6c719-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json'
+		) //fetch
+			.then(response => response.json()) //get response
+			.then(responseData => { 
+				const loadedIngredients = []; //stuur de data naar een array
+				for (const key in responseData) { //'for in' overloopt een object en gaat hier naar een array 
+					loadedIngredients.push({
+						id: key,
+						title: responseData[key].title,
+						amount: responseData[key].amount,
+					});
+				}
+				setUserIngredients(loadedIngredients);
+			});
+	}, []); /* Zonder deze lege array komen we in een indefinite loop. Deze lege array geeft aan deze useEffect maar één keer mag gerendered worden. In de array kan een conditie gezet worden om aan te geven wanneerer kan gerendered worden. Leeg is hetzelfde als eenmaal, na de eerste render.  */
 
+  useEffect(() => {
+    console.log('RENDERING INGREDIENTS', userIngredients);
+  }, [userIngredients]); // verandert enkel bij verandering userIngredients
+
+	/* useEffect wordt uitgevoerd na het renderen van de component */
 	const addIngredientHandler = ingredient => {
 		fetch(
 			'https://react-learning-hooks-6c719-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json',
-      /* achter de url door Firebase gegenereerd, ingredients.json toevoegen */
+			/* achter de url door Firebase gegenereerd, ingredients.json toevoegen */
 			{
 				method: 'POST',
 				body: JSON.stringify(ingredient),
-				headers: { 'Content-Type': 'application/json' }
+				headers: { 'Content-Type': 'application/json' },
 			}
 		)
 			.then(response => {
