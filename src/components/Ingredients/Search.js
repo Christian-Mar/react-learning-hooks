c0fda10603ca945e4cc86caafbc2c394f4495351
Christian <71664957+Christian-Mar@users.noreply.github.com>
2021-08-25 20:55:28 +0200

    useRef + clean up with useEffect

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,29 +6,38 @@ import './Search.css';
 const Search = React.memo(props => {
 	const { onLoadIngredients } = props;
 	const [enteredFilter, setEnteredFilter] = useState('');
+	const inputRef = useRef();
 
 	useEffect(() => {
-		const query =
-			enteredFilter.length === 0
-				? ''
-				: `?orderBy="title"&equalTo="${enteredFilter}"`;
-		fetch(
-			'https://react-learning-hooks-6c719-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json' + query
-		) //fetch
-			.then(response => response.json()) //get response
-			.then(responseData => {
-				const loadedIngredients = []; //stuur de data naar een array
-				for (const key in responseData) {
-					//'for in' overloopt een object en gaat hier naar een array
-					loadedIngredients.push({
-						id: key,
-						title: responseData[key].title,
-						amount: responseData[key].amount,
+		const timer = setTimeout(() => {
+			if (enteredFilter === inputRef.current.value) {
+				const query =
+					enteredFilter.length === 0
+						? ''
+						: `?orderBy="title"&equalTo="${enteredFilter}"`;
+				fetch(
+					'https://react-learning-hooks-6c719-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json' +
+						query
+				)
+					.then(response => response.json()) //get response
+					.then(responseData => {
+						const loadedIngredients = []; //stuur de data naar een array
+						for (const key in responseData) {
+							//'for in' overloopt een object en gaat hier naar een array
+							loadedIngredients.push({
+								id: key,
+								title: responseData[key].title,
+								amount: responseData[key].amount,
+							});
+						}
+						onLoadIngredients(loadedIngredients);
 					});
-				}
-				onLoadIngredients(loadedIngredients);
-			});
-	}, [enteredFilter, onLoadIngredients]);
+			}
+		}, 500);
+		return () => {
+      clearTimeout(timer); //clean up -> één timer over, de vorig wordt opgeruimd
+    };
+	}, [enteredFilter, onLoadIngredients, inputRef]);
 
 	return (
 		<section className='search'>
@@ -36,6 +45,7 @@ const Search = React.memo(props => {
 				<div className='search-input'>
 					<label>Filter by Title</label>
 					<input
+						ref={inputRef}
 						type='text'
 						value={enteredFilter}
 						onChange={event => setEnteredFilter(event.target.value)}
