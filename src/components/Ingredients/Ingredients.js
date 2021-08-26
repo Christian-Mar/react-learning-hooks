b00978d5ad2b21e2cddc47d8a9d4a12c32f1ff0a
Christian <71664957+Christian-Mar@users.noreply.github.com>
2021-08-26 21:12:29 +0200

    another useReducer

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useState, useEffect, useCallback } from 'react';
+import React, { useReducer, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -18,11 +18,27 @@ const ingredientReducer = (currentIngredients, action) => {
   }
 }
 
+const httpReducer = (curHttpState, action) => {
+  switch (action.type) {
+    case 'SEND':
+      return { loading: true, error: null };
+    case 'RESPONSE':
+      return { ...httpState, loading: false };
+    case 'ERROR':
+      return { loading: false, error: action.errorMessage };
+    case 'CLEAR':
+      return { ...curHttpState, error: null };
+      default:
+        throw new Error('Should not be reached!')
+  }
+}
+
 const Ingredients = () => {
   const [userIngredients, dispatch] = useReducer(ingredientReducer, [])
+  const [httpState, dispatchHttp] = useReducer(httpReducer, { loading: false, error: null });
 	//const [userIngredients, setUserIngredients] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState();
+	//const [isLoading, setIsLoading] = useState(false);
+	//const [error, setError] = useState();
 	useEffect(() => {
 		console.log('RENDERING INGREDIENTS', userIngredients);
 	}, [userIngredients]); // verandert enkel bij verandering userIngredients
@@ -35,7 +51,7 @@ const Ingredients = () => {
 	}, []);
 
 	const addIngredientHandler = ingredient => {
-		setIsLoading(true);
+		dispatchHttp({type: 'SEND'});
 		fetch(
 			'https://react-learning-hooks-6c719-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json',
 			/* achter de url door Firebase gegenereerd, ingredients.json toevoegen */
@@ -46,7 +62,7 @@ const Ingredients = () => {
 			}
 		)
 			.then(response => {
-				setIsLoading(false);
+				dispatchHttp({type: 'RESPONSE'});
 				return response.json();
 			})
 			.then(responseData => {
@@ -62,7 +78,7 @@ const Ingredients = () => {
 	};
 
 	const removeIngredientHandler = ingredientId => {
-		setIsLoading(true);
+		dispatchHttp({type: 'SEND'});
 		fetch(
 			`https://react-learning-hooks-6c719-default-rtdb.europe-west1.firebasedatabase.app/ingredients/${ingredientId}.json`,
 
@@ -71,29 +87,28 @@ const Ingredients = () => {
 			}
 		)
 			.then(response => {
-				setIsLoading(false);
+				dispatchHttp({type: 'RESPONSE'});
 				//setUserIngredients(prevIngredients =>
 				//	prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
 				//);
         dispatch({type: 'DELETE', id: ingredientId})
 			})
 			.catch(error => {
-				setError('Something went wrong!');
-        setIsLoading(false);
+				dispatchHttp({type: 'ERROR', errorMessage: 'Something went wrong!' });
 			});
 	};
 
   const clearError = () => {
-    setError(null);
+    dispatchHttp({ type: 'CLEAR' });
     
   }
 
 	return (
 		<div className='App'>
-			{error  && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+			{httpState.error  && <ErrorModal onClose={clearError}>{httpState.error}</ErrorModal>}
 			<IngredientForm
 				onAddIngredient={addIngredientHandler}
-				loading={isLoading}
+				loading={httpState.loading}
 			/>
 
 			<section>
